edf3e3e3e49556b02cf3852339953dae6ca5aed0
Ajay Susanth <ajaysusanth10@gmail.com>
2024-07-23 21:44:29 +0530

    edit-delete

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,6 +1,51 @@
+import { useState } from "react"
+import BackButton from "../components/BackButton"
+import { useNavigate,useParams } from "react-router-dom"
+import axios from 'axios'
+import Spinner from "../components/Spinner"
+
+
 const DeleteBook = () => {
+  const [loading,setLoading] = useState(false)
+  const navigate = useNavigate()
+  const {id} = useParams()
+
+  const handleDeleteBook = ()=>{
+    setLoading(true)
+    axios.delete(`http://localhost:5555/books/${id}`)
+    .then((res)=>{
+      setLoading(false)
+      navigate('/')
+    })
+    .catch((err)=>{
+      setLoading(false)
+      alert('Some error occured')
+      console.log(err)
+    })
+  }
+
+
   return (
-    <div>DeleteBook</div>
+    <div className='p-4'>
+      <BackButton/>
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {
+        loading ? <Spinner/> : ''
+      }
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl p-8 w-[600px] mx-auto">
+        <h3 className="text-2xl">
+          Are you sure you want to delete ?
+        </h3>
+
+        <button
+        className="bg-red-600 text-white m-8 w-full p-4"
+        onClick={handleDeleteBook}
+        >
+          Yes
+        </button>
+      </div>
+
+    </div>
   )
 }
 export default DeleteBook
